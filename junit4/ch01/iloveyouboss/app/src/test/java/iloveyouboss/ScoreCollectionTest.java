9688eecc4136843405e92fc78c7b4869b683a34f
gomahera-mymoota <iceman4u@naver.com>
2024-07-26 17:31:18 +0900

    ex04: 테스트 Arrange, Act, Assert

--- junit4/ch01/iloveyouboss/app/src/test/java/iloveyouboss/ScoreCollectionTest.java
+++ junit4/ch01/iloveyouboss/app/src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -1,11 +1,27 @@
 package iloveyouboss;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class ScoreCollectionTest {
     
     @Test
-    public void test() {
-        
+    public void answersArithmeticMeanOfTwoNumbers() {
+        // 준비: arrange
+        var collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // 실행: act
+        var actualResult = collection.arithmeticMean();
+
+        // 단언: assert
+        assertThat(actualResult, equalTo(6));     // deprecated
+        assertThat(actualResult, is(6));            // deprecated
+
+        assertEquals(actualResult, 6);        
     }
 }
