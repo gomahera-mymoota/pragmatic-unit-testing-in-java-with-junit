efdfe0f9b91dbfc30f02cfe40efb5ce438ceac87
gomahera-mymoota <iceman4u@naver.com>
2024-07-29 11:14:04 +0900

    ex02: 실패하는 테스트 작성

---
+++ junit5/ch01/iloveyouboss/app/src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,13 @@
+package iloveyouboss;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class ScoreCollectionTest {
+    
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+    
+}
