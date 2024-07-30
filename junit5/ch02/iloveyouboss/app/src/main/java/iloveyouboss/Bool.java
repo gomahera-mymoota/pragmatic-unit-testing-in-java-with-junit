7d86706c949e04dab38d7639237d847e84c8e760
gomahera-mymoota <iceman4u@naver.com>
2024-07-30 17:57:00 +0900

    p043: 테스트 대상 이해: Profile 클래스

---
+++ junit5/ch02/iloveyouboss/app/src/main/java/iloveyouboss/Bool.java
@@ -0,0 +1,14 @@
+package iloveyouboss;
+
+public enum Bool {
+    
+    False(0),
+    True(1);
+
+    public static final int FALSE = 0;
+    public static final int TRUE = 1;
+    private final int value;
+    private Bool(int value) { this.value = value; }
+    public int getValue() { return value; }
+
+}
