b84da7d556cedd417eab42647eea9af5592e600d
gomahera-mymoota <iceman4u@naver.com>
2024-07-31 14:03:46 +0900

    p052: @BeforeEach 메서드로 테스트 초기화

--- junit5/ch02/iloveyouboss/app/src/test/java/iloveyouboss/ProfileTest.java
+++ junit5/ch02/iloveyouboss/app/src/test/java/iloveyouboss/ProfileTest.java
@@ -2,21 +2,30 @@ package iloveyouboss;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProfileTest {
 
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @BeforeEach
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
+
     @Test
     @DisplayName("머스트 매치 조건이 만족하지 않을 때 답은 FALSE")
     public void testMatchAnswersFalseWhenMustMatchCriteriaNotMet() {
         // Arrange
-        var profile = new Profile("Bull Hockey, Inc.");
-        var question = new BooleanQuestion(1, "Got bonuses?");
         var profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        var criteria = new Criteria();
         var criteriaAnswer = new Answer(question, Bool.TRUE);
         var criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
 
@@ -32,12 +41,9 @@ public class ProfileTest {
     @Test
     @DisplayName("돈 케어가 하나라도 있으면 답은 TRUE")
     public void testMatchAnswersTrueForAnyDontCareCriteria() {
-        var profile = new Profile("Bull Hockey, Inc.");
-        var question = new BooleanQuestion(1, "Got milk?");
         var profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        var criteria = new Criteria();
         var criteriaAnswer = new Answer(question, Bool.TRUE);
         var criterion = new Criterion(criteriaAnswer, Weight.DontCare);
         criteria.add(criterion);
