c9c5806aebb991f8d56e40b9c872c2bed1d8ff97
gomahera-mymoota <iceman4u@naver.com>
2024-08-02 13:47:11 +0900

    p062: assertThat은 명확한 값을 비교

--- junit5/ch03/scratch/app/src/test/java/scratch/AssertTest.java
+++ junit5/ch03/scratch/app/src/test/java/scratch/AssertTest.java
@@ -1,5 +1,9 @@
 package scratch;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,7 +29,40 @@ public class AssertTest {
         var initialBalance = account.getBalance();
         account.deposit(100);
 
-        assertTrue(account.getBalance() > initialBalance);
+        assertTrue(account.getBalance() > initialBalance);   // JUnit5
+
+        assertEquals(100, account.getBalance());    // JUnit5, 정확한 값으로 단언하는 것이 좋다
+        
+        assertThat(account.getBalance()).isEqualTo(100);    // assertJ
+    }
+
+    @Test
+    public void testDepositIncreasesBalance_hamcrestAssertTrue() {
+        account.deposit(50);
+
+        org.hamcrest.MatcherAssert.assertThat(account.getBalance() > 0, is(true));       // 장황하다
+
+        assertTrue(account.getBalance() > 0);                   // 이게 낫고
+        assertTrue(account.hasPositiveBalance());               // 이게 더 낫다
+        
+        assertThat(account.hasPositiveBalance()).isTrue();      // assertJ 방식
+    }
+
+    @Test
+    public void testAssertFailure() {
+        // 아래 testMatchesFailure()의 에러 메시지와 비교
+        assertTrue(account.getName().startsWith("xyz"));
+    }
+
+    @Test
+    public void testMatchesFailure() {
+        // 아래 testAssertJMatchesFailure()의 가독성 및 에러 메시지와 비교
+        org.hamcrest.MatcherAssert.assertThat(account.getName(), startsWith("xyz"));
+    }
+    
+    @Test
+    public void testAssertJMatchesFailure() {
+        assertThat(account.getName()).startsWith("xyz");
     }
 
 }
